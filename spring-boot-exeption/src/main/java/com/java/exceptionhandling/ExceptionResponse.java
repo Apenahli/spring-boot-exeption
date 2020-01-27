c2b5fe3f5cec595c1ea7ec5354876efbd6b9fdd7
apenahli <apenahli001@gmail.com>
2020-01-27 10:29:46 +0400

    add new status

---
+++ spring-boot-exeption/src/main/java/com/java/exceptionhandling/ExceptionResponse.java
@@ -0,0 +1,29 @@
+package com.java.exceptionhandling;
+
+public class ExceptionResponse {
+
+	private String errorMessage;
+	private String requestedURI;
+
+	public String getErrorMessage() {
+		return errorMessage;
+	}
+
+	public void setErrorMessage(final String errorMessage) {
+		this.errorMessage = errorMessage;
+	}
+
+	public String getRequestedURI() {
+		return requestedURI;
+	}
+
+	public void callerURL(final String requestedURI) {
+		this.requestedURI = requestedURI;
+	}
+
+	@Override
+	public String toString() {
+		return "ExceptionResponse [errorMessage=" + errorMessage + ", requestedURI=" + requestedURI + "]";
+	}
+
+}
